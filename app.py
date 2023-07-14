3cbca784e90150bb3e93267e36cdb81dadc3f694
paddyokore <paddyokore39@gmail.com>
2023-07-14 05:49:19 +0300

    new upp, requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import streamlit as sl
 from predict_page import show_predict_page
-from explore_page import show_explore_page
 import warnings
 warnings.filterwarnings('ignore')
 
-page = sl.sidebar.selectbox("Predict or Explore", ("Predict", "Explore"))
 
-if page == "Predict":
-    show_predict_page()
-else:
-    show_explore_page()
+show_predict_page()
